32c9b54f201dbd61119f60a5286df8d6422d67f0
Antonio Carvalho <antoniorcn@hotmail.com>
2026-02-24 21:08:26 -0300

    3esoa aula 01

---
+++ 3esoa/aula01/app/src/main/java/edu/curso/agendacontato/Contato.kt
@@ -0,0 +1,8 @@
+package edu.curso.agendacontato
+
+data class Contato (
+    val nome: String,
+    val telefone: String,
+    val email: String,
+    val descricao: String
+)
---
+++ 3esoa/aula01/app/src/main/java/edu/curso/agendacontato/MainActivity.kt
@@ -0,0 +1,114 @@
+package edu.curso.agendacontato
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import edu.curso.agendacontato.ui.theme.AgendaContatoTheme
+
+
+// var nome : String = ""
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val lista = mutableListOf<Contato>()
+
+        fun onGravar( nome : String, telefone : String, email : String, descricao : String ) {
+            val contato = Contato(nome, telefone, email, descricao)
+            lista.add( contato )
+        }
+
+        fun onPesquisar( nome : String ) : Contato? {
+            for (contato in lista) {
+                if (contato.nome.contains(nome)) {
+                    return contato
+                }
+            }
+            return null
+        }
+
+        setContent {
+            AgendaContatoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ContatoFormulario( modifier = Modifier.padding(innerPadding),
+                        gravar = {nome, telefone, email, descricao ->
+                            onGravar( nome, telefone, email, descricao ) },
+                        pesquisar = { nome -> onPesquisar( nome ) })
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ContatoFormulario( modifier : Modifier = Modifier,
+                       gravar : ( String, String, String, String ) -> Unit,
+                       pesquisar : (String) -> Contato? ) {
+    var nome by remember { mutableStateOf("") }
+    var telefone by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var descricao by remember { mutableStateOf("") }
+
+    Column( modifier = modifier ) {
+        Text("Agenda de Contatos", modifier = Modifier
+            .background(Color.Cyan)
+            .padding( 20.dp)
+            .align(Alignment.CenterHorizontally), fontSize = 32.sp)
+        Text("Nome Completo: ")
+        TextField(value = nome, onValueChange = { nome = it })
+        Text("Telefone: ")
+        TextField(value = telefone, onValueChange = { telefone = it })
+        Text("Email: ")
+        TextField(value = email, onValueChange = { email = it })
+        Text("Descricao: ")
+        TextField(value = descricao, onValueChange = { descricao = it })
+
+        Row() {
+            Button( onClick = {
+                gravar( nome, telefone, email, descricao )
+                nome= ""
+                telefone = ""
+                email = ""
+                descricao = ""
+            }) {
+                Text("Gravar")
+            }
+
+            Button( onClick = {
+                val contato = pesquisar( nome )
+                if (contato != null) {
+                    nome = contato.nome
+                    telefone = contato.telefone
+                    email = contato.email
+                    descricao = contato.descricao
+                }
+            }) {
+                Text("Pesquisar")
+            }
+        }
+    }
+}

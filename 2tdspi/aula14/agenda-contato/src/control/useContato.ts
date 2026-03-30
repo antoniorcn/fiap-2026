080db8b1b563b336805e52d6b18e1954067aeb1e
Antonio Carvalho <antoniorcn@hotmail.com>
2026-03-30 11:14:22 -0300

    2tdspi aula14 parte 1

---
+++ 2tdspi/aula14/agenda-contato/src/control/useContato.ts
@@ -0,0 +1,113 @@
+import { useState, useEffect } from "react";
+import { useColorScheme } from "react-native";
+import {Contato, contatoSchema} from "../model/contato";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const useContato = ( mensagem : ( msg: string ) => void ) => {
+      const colorScheme = useColorScheme();
+      const [lista, setLista] = useState<Contato[]>([]);  
+      const [isDark, setDark] = useState<boolean>(
+      colorScheme == "dark" ? true : false
+      );
+    
+      const [filtro, setFiltro] = useState<string>("");
+      const [carregando, setCarregando] = useState<boolean>(false);
+      const [contador, setContador] = useState<number>(0);
+
+      const [nomeErro, setNomeErro] = useState<string | null>(null);
+      const [telefoneErro, setTelefoneErro] = useState<string | null>(null);
+      const [emailErro, setEmailErro] = useState<string | null>(null);
+       
+      const listaFiltrada = lista
+      .filter(  (objContato : Contato, idx : number) => {
+        return objContato.nome.includes( filtro )
+      } );
+    
+      const salvar = ( contato : Contato ) => {
+        setNomeErro(null);
+        setTelefoneErro(null);
+        setEmailErro(null);
+        contatoSchema.validate(contato, {abortEarly: false})
+        .then( ()=> {
+            const novoContador = contador + 1;
+            setLista( ( listaAntiga : Contato[] )=>{
+              contato.id = novoContador;
+              const novaLista = [...listaAntiga, contato];
+              const novaListaSerializada = JSON.stringify( novaLista );      
+              AsyncStorage.setItem("LISTA_CONTATOS", novaListaSerializada);
+              AsyncStorage.setItem("CONTADOR", novoContador.toString() );
+              return novaLista;
+            } );
+            setContador( novoContador );
+            mensagem("Contato Salvo");
+        }).catch( (erros : any ) => {
+          for (const erro of erros.inner) {
+            console.log( erro.path, erro.message, erro );
+            // mensagem(erro.path + " - " + erro.message);
+            if (erro.path == "nome") {
+              setNomeErro(erro.message);
+            } else if (erro.path == "telefone") {
+              setTelefoneErro(erro.message);
+            } else if (erro.path == "email") {
+              setEmailErro(erro.message);
+            }
+          }
+          mensagem("Erro ao salvar o contato");
+        })
+      } 
+    
+      const pesquisar = ( nome : string ) : Contato | null => {
+        console.log("Pesquisar acionado", lista);
+        for(const contato of lista) { 
+          console.log("Contato: ", contato);
+          if( contato.nome.includes( nome )) { 
+            return contato;
+          }
+        }
+        return null;
+      }
+
+      const apagar = ( contato : Contato ) => {
+        const novaLista = lista.filter( ( item : Contato ) => 
+          contato.id != item.id
+        );
+        setLista( novaLista );
+        const novaListaSerializada = JSON.stringify( novaLista );      
+        AsyncStorage.setItem("LISTA_CONTATOS", novaListaSerializada);
+      }
+    
+    
+      const onRefresh = async () => { 
+        setCarregando(true);
+        try { 
+          const strContador = await AsyncStorage.getItem("CONTADOR");
+          const strLista = await AsyncStorage.getItem("LISTA_CONTATOS");
+          if (strContador != null) {
+            setContador( parseInt(strContador) + 1);
+          }
+          if (strLista != null) {
+            setLista( JSON.parse(strLista) );
+          }
+        } catch ( err ) { 
+          mensagem("Erro ao carregar os dados");
+        }
+        setCarregando(false);
+      }
+    
+      useEffect( ()=>{
+        onRefresh();
+      }, [])
+
+      return { 
+        isDark, setDark, 
+        lista, setLista, 
+        listaFiltrada,
+        filtro, setFiltro, 
+        contador, setContador,
+        salvar, pesquisar, apagar, onRefresh,
+        carregando,
+        nomeErro, telefoneErro, emailErro, 
+      }
+}
+
+export {useContato};
